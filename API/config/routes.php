ce11d7ad965dd3dd38b97a7d64c355655e41641c
NicoRiri <nicolate.bernardet@gmail.com>
2023-10-23 12:59:15 +0200

    routes + actions

---
+++ API/config/routes.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+return function( \Slim\App $app):void {
+
+    $app->post('/api/connexiontoken[/]', \NRV\api\actions\ConnexionTokenAction::class)
+        ->setName('connexionToken');
+    $app->post('/api/connexionbasic[/]', \NRV\api\actions\ConnexionBasicAction::class)
+        ->setName('connexionBasic');
+    $app->get('/api/validetoken[/]', \NRV\api\actions\ValideTokenAction::class)
+        ->setName('spectacles');
+    $app->get('/api/spectacle[/]', \NRV\api\actions\GetSpectacleAction::class)
+        ->setName('spectacles');
+    $app->get('/api/spectacle/{id}[/]', \NRV\api\actions\GetSpectacleByIdAction::class)
+        ->setName('spectacle');
+    $app->get('/api/soiree[/]', \NRV\api\actions\GetSoireeAction::class)
+        ->setName('soirees');
+    $app->get('/api/soiree/{id}[/]', \NRV\api\actions\GetSoireeByIdAction::class)
+        ->setName('soiree');
+    $app->get('/api/profile[/]', \NRV\api\actions\GetProfileAction::class)
+        ->setName('profile');
+    $app->post('/api/achat[/]', \NRV\api\actions\PostAchatAction::class)
+        ->setName('achat');
+};
---
+++ API/src/actions/GetSpectacleAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NRV\api\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class GetSpectacleAction extends AbstractAction
+{
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
