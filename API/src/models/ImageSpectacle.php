82577c43b03bc72e7f5368f4c18dafc2fe2b537c
NicoRiri <nicolate.bernardet@gmail.com>
2023-10-23 12:54:06 +0200

    Models

---
+++ API/src/models/ImageSpectacle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NRV\api\models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ImageSpectacle extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'Utilisateur';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function spectacles(): BelongsTo
+    {
+        return $this->belongsTo(Spectacle::class);
+    }
+}
