fc52c10d008604c367713dbff2ecc2174861c9f6
Exelupra <101408547+Exelupra@users.noreply.github.com>
2023-10-26 15:00:20 +0200

    Profil +JS encore a modifier

---
+++ front/profil/profill.js
@@ -0,0 +1,77 @@
+
+// On récupère le token stocké dans le local storage
+let token = localStorage.getItem('token');
+
+// URL de l'API
+const apiUrl2 = 'http://docketu.iutnc.univ-lorraine.fr:42769/api/soiree';
+const apiUrl = 'http://docketu.iutnc.univ-lorraine.fr:42769/api/profile';
+
+// Configuration de l'en-tête de la requête avec les tokens
+const headers = new Headers();
+headers.append('Authorization', `Bearer ${token}`);
+
+const fetchOptions = {
+    method: 'GET',
+    headers: headers,
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(apiUrl, fetchOptions)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json(); // Convertir la réponse en JSON
+            } else {
+                throw new Error('Échec de la requête Fetch');
+            }
+        })
+        .then(data => {
+            console.log('Données récupérées avec succès:', data);
+            const profile = data.profile;
+            const nom = profile.nom;
+            const prenom = profile.prenom;
+            const email = profile.email;
+            const billets = data.billets;
+            const tableBillets = document.getElementById("table-billets");
+
+            function getSoireeDetails(soireeId) {
+                fetch(`${apiUrl2}/${soireeId}`)
+                    .then(response => {
+                        if (response.status === 200) {
+                            return response.json();
+                        } else {
+                            throw new Error('Échec de la requête Fetch pour la soirée');
+                        }
+                    })
+                    .then(soireeData => {
+                        const row = tableBillets.insertRow();
+                        row.insertCell(0).textContent = soireeData.nom; // Nom de la soirée
+                        row.insertCell(1).textContent = soireeData.thematique; // Thématique de la soirée
+                        row.insertCell(2).textContent = soireeData.date; // Date de la soirée
+                        row.insertCell(3).textContent = soireeData.horaire; // Horaire de la soirée
+                    })
+                    .catch(error => {
+                        console.error('Erreur lors de la récupération des détails de la soirée:', error);
+                    });
+            }
+
+            billets.forEach(billet => {
+                for (let i = 0; i < billet.quantiteAssise; i++) {
+                    getSoireeDetails(billet.soiree_id);
+                }
+                for (let i = 0; i < billet.quantiteDebout; i++) {
+                    getSoireeDetails(billet.soiree_id);
+                }
+            });
+
+            const nomElement = document.getElementById("nom");
+            const prenomElement = document.getElementById("prenom");
+            const emailElement = document.getElementById("email");
+
+            nomElement.textContent = nom;
+            prenomElement.textContent = prenom;
+            emailElement.textContent = email;
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
+});
