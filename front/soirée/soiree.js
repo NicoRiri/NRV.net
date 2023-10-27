b664b8fcef4911f1770cfcf0cf8b1da42d291fbb
oudin-clement <clementoudin3@gmail.com>
2023-10-27 15:20:58 +0200

    changement des boutons en liens

--- front/soirée/soiree.js
+++ front/soirée/soiree.js
@@ -11,8 +11,9 @@ fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/soiree/")
         document.getElementsByTagName("h2")[0].innerText = "Theme : " + soiree.thematique;
         document.getElementsByTagName("h2")[1].innerText = soiree.date;
         document.getElementsByTagName("h3")[0].innerText = "Billet à " + soiree.prixPlace + "€";
+        document.getElementsByTagName("iframe")[0].src = soiree.lieu_id.lien;
         for (let i = 0; i < soiree.spectacleArrayId.length; i++) {
-            fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/spectacle/"+soiree.spectacleArrayId[i])
+            fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/spectacle/" + soiree.spectacleArrayId[i])
                 .then(response => {
                     if (!response.ok) {
                         throw new Error(`Erreur HTTP! Statut: ${response.status}`);
@@ -28,20 +29,20 @@ fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/soiree/")
                     let video = spectacle.VideoUrl;
                     let artiste = "";
                     for (let j = 0; j < spectacle.artisteArray.length; j++) {
-                        artiste += spectacle.artisteArray[j].pseudonyme+ " ";
+                        artiste += spectacle.artisteArray[j].pseudonyme + " ";
                     }
-                    document.getElementsByTagName("body")[0].innerHTML+=`
+                    document.getElementsByTagName("body")[0].innerHTML += `
                         <div class="spectacle box">
-                            <img src="`+ imgsrc +`">
+                            <img src="` + imgsrc + `">
                                 <div>
                                     <div>
-                                        <h3>`+ titre+`</h3>
-                                        <h3>`+ horaire+`</h3>
+                                        <h3>` + titre + `</h3>
+                                        <h3>` + horaire + `</h3>
                                     </div>
-                                    <p> `+ description + `</p>
-                                    <h4>artiste(s): `+ artiste +`</h4>
+                                    <p> ` + description + `</p>
+                                    <h4>artiste(s): ` + artiste + `</h4>
                                 </div>
-                                <iframe width="560" height="315" src="`+video+`" allowfullscreen></iframe>
+                                <iframe width="560" height="315" src="` + video + `" allowfullscreen></iframe>
 
                         </div>`;
                 });
@@ -49,3 +50,6 @@ fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/soiree/")
 
 
     });
+
+
+
