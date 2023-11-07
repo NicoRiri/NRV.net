e365af0af73872f4c52a91bce13c9ebd4ad71a9d
Amaury258 <demq.amaury@gmail.com>
2023-11-07 18:16:13 +0100

    Correction de bug d'affichage)

--- front/catalogue/cat.js
+++ front/catalogue/cat.js
@@ -21,95 +21,8 @@
         return soiree;
     }
 
-    let specs = fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/spectacle/")
-    .then(function(response) {
-        if (!response.ok) {
-        throw new Error("Réponse réseau incorrecte");
-        }
-        return response.json();
-    });
-    
-    specs.then(data => {
-            data.spectacle.forEach(element => {
-            let a = document.createElement("a");
-            a.setAttribute("href", "../detailspectacle/index.html?id=" + element.details.id);
-            a.setAttribute("class", "item");
-            let html = `<div class="text">
-                <h2>${element.details.titre}</h2>`;
-
-                element.details.artisteArray.forEach(artiste => {
-                    html += `<p>${artiste.pseudonyme}</p>`;
-                });
-                
-                getSoireeById(element.details.soiree_id)
-                .then(soiree => {
-                    html += `<p>${soiree.soiree.details.date}</p>
-                                    <p>${soiree.soiree.details.nbPlaceRestanteTotale} places</p>`;
-                });
-
-            html += `</div>`;
-                if(element.details.tabImg.length > 1){
-                    let rand = Math.floor(Math.random() * element.details.tabImg.length);
-                    html += `<img src="${element.details.tabImg[rand].imgUrl}" alt="Image 1" class="image"/>`;
-                } else {
-                    html += `<img src="${element.details.tabImg[0].imgUrl}" alt="Image 1" class="image"/>`;
-                }
-            a.innerHTML = html;
-            document.getElementById("catalogue").appendChild(a);
-        });
-    })
-    .catch(function(error) {
-        console.log(
-        "Il y a eu un problème avec l'opération fetch: " +
-            error.message
-        );
-    });
-
-    soirees.then(data => {
-        data.soiree.forEach(element => {
-            let lieu = document.createElement("option");
-            lieu.setAttribute("value", element.lieu_id.nom);
-            lieu.innerHTML = element.lieu_id.nom;
-            document.getElementById("lieux").appendChild(lieu);
-            
-            let theme = document.createElement("option");
-            theme.setAttribute("value", element.thematique);
-            theme.innerHTML = element.thematique;
-            document.getElementById("genre").appendChild(theme);
-        });
-    });
-
-/* Les Event */
-
-    document.getElementById("txtfiltre").addEventListener("click", function(){
-        document.getElementById("filtre").classList.toggle("filtreshown");
-        document.getElementById("hidden").classList.toggle("hiddenshown");
-
-    });
-
-    document.getElementById("exec").addEventListener("click", function(){
-        document.getElementById("catalogue").innerHTML = "";
-        let lieu = document.getElementById("lieux").value;
-        let theme = document.getElementById("genre").value;
-        let date = document.getElementById("date").value;
-        specs.then(data => {
-            data.spectacle.forEach(element => {
-                let arr = [];
-                getSoireeById(element.details.soiree_id).then(soiree => {
-                    let lieusoiree = soiree.soiree.details.lieu_id.nom == lieu;
-                    let themesoiree = soiree.soiree.details.thematique == theme;
-                    let datesoiree = soiree.soiree.details.date == date;
-
-                    if(lieusoiree || themesoiree || datesoiree){
-                        arr.push(element.details.titre);
-                    } else {
-                        if(lieu == "0" && theme == "0" && date == ""){
-                            arr.push(element.details.titre);
-                        }
-                    }
-                    
-                    arr.forEach(item => {
-                        let a = document.createElement("a");
+    function createAElement(element, soiree){
+        let a = document.createElement("a");
                             a.setAttribute("href", "../detailspectacle/index.html?id=" + element.details.id);
                             a.setAttribute("class", "item");
                             a.innerHTML = `<div class="text">
@@ -136,7 +49,81 @@
                                     a.innerHTML += `<img src="${element.details.tabImg[0].imgUrl}" alt="Image 1" class="image"/>`;
                                 }
                             document.getElementById("catalogue").appendChild(a); 
-                    });
+                            }
+
+    let specs = fetch("http://docketu.iutnc.univ-lorraine.fr:42769/api/spectacle/")
+    .then(function(response) {
+        if (!response.ok) {
+        throw new Error("Réponse réseau incorrecte");
+        }
+        return response.json();
+    });
+    
+    specs.then(data => {
+            data.spectacle.forEach(element => {
+                getSoireeById(element.details.soiree_id)
+                .then(soiree => {
+                    if(soiree.soiree.details.nbPlaceRestanteTotale > 0){
+                        createAElement(element, soiree);
+                    }
+                });
+        });
+    })
+    .catch(function(error) {
+        console.log(
+        "Il y a eu un problème avec l'opération fetch: " +
+            error.message
+        );
+    });
+
+    soirees.then(data => {
+        data.soiree.forEach(element => {
+            let lieu = document.createElement("option");
+            lieu.setAttribute("value", element.lieu_id.nom);
+            lieu.innerHTML = element.lieu_id.nom;
+            document.getElementById("lieux").appendChild(lieu);
+            
+            let theme = document.createElement("option");
+            theme.setAttribute("value", element.thematique);
+            theme.innerHTML = element.thematique;
+            document.getElementById("genre").appendChild(theme);
+        });
+    });
+
+
+
+/* Les Event */
+
+    document.getElementById("txtfiltre").addEventListener("click", function(){
+        document.getElementById("filtre").classList.toggle("filtreshown");
+        document.getElementById("hidden").classList.toggle("hiddenshown");
+
+    });
+
+    document.getElementById("exec").addEventListener("click", function(){
+        document.getElementById("catalogue").innerHTML = "";
+        let lieu = document.getElementById("lieux").value;
+        let theme = document.getElementById("genre").value;
+        let date = document.getElementById("date").value;
+        specs.then(data => {
+            data.spectacle.forEach(element => {
+                getSoireeById(element.details.soiree_id).then(soiree => {
+                    let lieusoiree = soiree.soiree.details.lieu_id.nom == lieu;
+                    let themesoiree = soiree.soiree.details.thematique == theme;
+                    let datesoiree = soiree.soiree.details.date == date;
+                    let nbplacesoiree = soiree.soiree.details.nbPlaceRestanteTotale > 0;
+
+                    if(lieusoiree || themesoiree || datesoiree){
+                        if(nbplacesoiree){
+                            createAElement(element, soiree);
+                        }
+                    } else {
+                        if(lieu == "0" && theme == "0" && date == ""){
+                            if(nbplacesoiree){
+                                createAElement(element, soiree);
+                            }
+                        }
+                    }
                 });
             });
         });
